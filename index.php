cd0d2897c511d679c51e4127b505958dec0f733b
orlinwilliams <orlinwilliams0077@gmail.com>
2021-03-05 00:11:18 -0600

    add header

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="vendor/twbs/bootstrap/dist/css/bootstrap.css">  
+  <link rel="stylesheet" href="views/css/style.css">
   <title>AFP</title>
 </head>
 <body>
