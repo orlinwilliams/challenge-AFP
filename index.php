c2da46c5d36a0d47449122eab0bb828897ffc150
orlinwilliams <orlinwilliams0077@gmail.com>
2021-03-04 23:21:11 -0600

    initing proyect

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="vendor/twbs/bootstrap/dist/css/bootstrap.css">  
+  <title>AFP</title>
+</head>
+<body>
+  <?php include 'views/header.php'?> 
+  <script src="vendor/twbs/bootstrap/dist/js/bootstrap.js"></script>
+</body>
+</html>
---
+++ vendor/composer/installed.php
@@ -0,0 +1,49 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.0.0+no-version-set',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => 'orlin/challenge-afp',
+  ),
+  'versions' => 
+  array (
+    'orlin/challenge-afp' => 
+    array (
+      'pretty_version' => '1.0.0+no-version-set',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+    'phpmailer/phpmailer' => 
+    array (
+      'pretty_version' => 'v6.3.0',
+      'version' => '6.3.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '4a08cf4cdd2c38d12ee2b9fa69e5d235f37a6dcb',
+    ),
+    'twbs/bootstrap' => 
+    array (
+      'pretty_version' => 'v5.0.0-beta2',
+      'version' => '5.0.0.0-beta2',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'e50c11b8c6434b6d68ea5897771e4d35fe12f5c3',
+    ),
+    'twitter/bootstrap' => 
+    array (
+      'replaced' => 
+      array (
+        0 => 'v5.0.0-beta2',
+      ),
+    ),
+  ),
+);
